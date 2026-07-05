a98e587db329b358bb8cd1a4eec61cb666d89db3
salekyn <osamasalekin21@gmail.com>
2026-07-05 20:32:06 +0600

    Started documenting learning Java

--- src/prac1/p4.java
+++ src/prac1/p4.java
@@ -2,6 +2,52 @@ package prac1;
 import java.util.Scanner;
 public class p4 {
     static void main() {
+        //adding two matrices
+
+        int matrixOne [][] = new int[2][2];
+        int matrixTwo [][] = new int [2][2];
+        int matrixOutput [][] = new int [2][2];
+
+        for (int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                matrixOne[i][j] = (int)(Math.random()*11);
+                matrixTwo[i][j] = (int)(Math.random()*11);
+            }
+        }
+
+        for (int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                System.out.print(matrixOne[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                System.out.print(matrixTwo[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+        for (int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                matrixOutput [i][j] = matrixOne[i][j] + matrixTwo[i][j];
+            }
+        }
+
+        System.out.println();
+
+        for (int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                System.out.print(matrixOutput[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
 
     }
 }
